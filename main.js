e3311243bc053f5d2d2d2979990bcdd89c885295
DilmuhammadRustamov2014 <dilmuhammad.itf@gmail.com>
2025-01-31 09:52:28 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+
+// // const userAge = +prompt("enter you age")
+
+
+// // switch (userAge >= 18) {
+// //     case true:
+// //         console.log("oqishga kirdiz")
+// //         break;
+
+// //     default:
+// //         console.log("oqishga kirmadiz");
+        
+// //         break;
+// // }
+
+
+// const week = +prompt("1 dan 7 gacha son kiriting")
+
+// switch (week){
+//     case 1:
+//         console.log("Dushanba");
+//         break;
+//         case 2:
+//         console.log("Seshanba");
+//         case 3:
+//         console.log("chorshanba");
+//         break;
+//         case 4:
+//         console.log("payshamba");
+//         break;case 5:
+//         console.log("juma");
+//         break;
+//         case 6:
+//         console.log("shanba");
+//         break;
+//         case 7:
+//         console.log("yakshanba");
+//         break;
+//         default:
+//             console.log("Iltimosan 1 dan 7ga kiriting");
+// }
+
+
+
+
